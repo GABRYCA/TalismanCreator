a687646566a4028a25cfa883c9df0285ca2d6a0a
AnonymousGCA <gabrycaretti@gmail.com>
2022-01-05 13:25:43 +0100

    Initial commit

---
+++ src/main/java/me/gca/talismancreator/TalismanCreator.java
@@ -0,0 +1,16 @@
+package me.gca.talismancreator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TalismanCreator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
